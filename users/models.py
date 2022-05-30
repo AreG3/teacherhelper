923c5ce80cc7177f877c4d024136f1fd68a02b47
Arkadiusz <arek100j@wp.pl>
2022-05-31 00:37:51 +0200

    Mozna dodawac paczki lub pojedyncze pliki

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', verbose_name='Avatar')
 
     def __str__(self):
         return f'Profil: {self.user.username}'
